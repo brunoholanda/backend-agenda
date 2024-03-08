2252b2611ac8d1a95859f8288b912b0bf13d862c
brunoholanda <holanda_rodrigues@hotmail.com>
2024-03-08 18:54:52 -0300

    retirada de consoles

--- start/schedule.js
+++ start/schedule.js
@@ -8,7 +8,6 @@ CronJob.schedule('0 12 * * *', async () => {
   console.log('Executando a tarefa cron...');
 
   const today = moment().format('DD/MM/YYYY');
-    console.log('Data de hoje:', today);
 
     const agendamentosParaEmail = await Database
         .table('agendamentos')
@@ -19,10 +18,8 @@ CronJob.schedule('0 12 * * *', async () => {
         .where('companies.service_id', 3)
         .select('agendamentos.*', 'clients.client_email', 'companies.service_id');
 
-        console.log('Agendamentos encontrados:', agendamentosParaEmail.length);
 
     for (let agendamento of agendamentosParaEmail) {
-      console.log('Agendamento:', agendamento);
 
         await Mail.send('emails.pesquisa_satisfacao', {
             clienteNome: agendamento.nome,
